cf283182a1137d3d12ebd42c8db71ee5b0d3dd7e
janina <janina_bongon@dlsu.edu.ph>
2024-07-25 02:54:16 +0800

    camera activity no backend

---
+++ app/src/main/java/com/mobdeve/s11/group07/mco3/wanderfriend/CameraActivity.kt
@@ -0,0 +1,16 @@
+package com.mobdeve.s11.group07.mco3.wanderfriend
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CameraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_camera)
+
+    }
+}
